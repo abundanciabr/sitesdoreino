27fc66b93a5031c89ddec3d10c1e40dd0a01a6eb
abundanciabr <abundanciabr@gmail.com>
2026-09-07 21:25:35 -0300

    testes: rotas completas dos gestos da aula

--- services/cursos/tests/test_telas_da_sala.py
+++ services/cursos/tests/test_telas_da_sala.py
@@ -345,8 +345,20 @@ def test_cada_pausa_tem_o_segundo_e_um_formulario_proprio(
     )
     assert "Pausa 1 aos 1:30" in corpo
     assert "Pausa 2 aos 4:00" in corpo
-    assert corpo.count('action="' + reverse("registrar-pausa", args=["E00", 1])) == 1
-    assert corpo.count('action="' + reverse("registrar-pausa", args=["E00", 2])) == 1
+    assert (
+        corpo.count(
+            'action="'
+            + reverse("registrar-pausa-do-curso", args=["profissional", 1, "E00", 1])
+        )
+        == 1
+    )
+    assert (
+        corpo.count(
+            'action="'
+            + reverse("registrar-pausa-do-curso", args=["profissional", 1, "E00", 2])
+        )
+        == 1
+    )
     assert 'name="campo_0"' in corpo and 'name="campo_1"' in corpo
 
 
