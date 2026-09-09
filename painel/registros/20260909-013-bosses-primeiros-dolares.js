50cca8258390de35eb8b7d1c15e139464df35ec2
abundanciabr <abundanciabr@gmail.com>
2026-09-08 22:13:30 -0300

    painel: registrar publicacao dos Bosses

---
+++ painel/registros/20260909-013-bosses-primeiros-dolares.js
@@ -0,0 +1,15 @@
+(function(){ (window.REGISTROS = window.REGISTROS || []).push({
+  arquivo: "20260909-013-bosses-primeiros-dolares",
+  tipo: "nota",
+  quando: "2026-09-09",
+  titulo: "Publicação dos Bosses de Primeiros Dólares",
+  detalhe: "O PR #1454 prepara nova publicação da célula cursos após o merge dos 8 Bosses escolhidos para os módulos principais. O deploy executará o comando idempotente e exibirá os nomes marcados no resumo operacional.",
+  autoridade: "sessao",
+  evidencia: "https://github.com/abundanciabr/sitesdoreino/pull/1454",
+  verificado_em: "2026-09-09",
+  precisa_do_dono: false,
+  responde_a: null,
+  gravidade: "info",
+  frente: "cursos",
+  vence_em_dias: null
+});})();
