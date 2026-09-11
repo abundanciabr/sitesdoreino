dec644a10f628ff7a3e1b9b25d5eec45082b6dbc
abundanciabr <abundanciabr@gmail.com>
2026-09-11 11:31:11 -0300

    painel: comprovar primeira reconciliação dos cursos

--- painel/registros/20260911-067-primeira-reconciliacao-dos-dois-cursos.js
+++ painel/registros/20260911-067-primeira-reconciliacao-dos-dois-cursos.js
@@ -5,7 +5,7 @@
   titulo: "O pedido 1577 registra a primeira conferência dos dois cursos",
   detalhe: "A primeira reconciliação encontrou dois cursos cadastrados. Primeiros Dólares com Roblox tem 96 aulas, com uma aberta; Profissional tem 34, sem aula aberta. A primeira aula publicada reproduz o vídeo. Fontes privadas foram localizadas, mas ainda não são equivalências confirmadas de aulas. A reconciliação integral e a autorização de publicação continuam abertas; este registro apenas relata a conferência.",
   autoridade: "sessao",
-  evidencia: "https://github.com/abundanciabr/sitesdoreino/pull/1577",
+  evidencia: "https://meshcraft.top/admin/escola/cursos/; https://meshcraft.top/admin/escola/primeiros-dolares/aulas/; https://meshcraft.top/cursos/primeiros-dolares/parte-1/01; https://github.com/abundanciabr/sitesdoreino/pull/1577",
   verificado_em: "2026-09-11",
   precisa_do_dono: false,
   responde_a: null,
