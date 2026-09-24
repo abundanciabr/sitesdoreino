150d06cf8b60e7ba41264ee4290b9c732558032e
abundanciabr <abundanciabr@gmail.com>
2026-09-24 10:11:48 -0300

    painel: registra publicação do webhook Appmax

---
+++ painel/registros/20260924-025-webhook-appmax-publicado.js
@@ -0,0 +1,16 @@
+(function(){ (window.REGISTROS = window.REGISTROS || []).push({
+  arquivo: "20260924-025-webhook-appmax-publicado",
+  tipo: "medicao",
+  quando: "2026-09-24",
+  titulo: "Webhook Appmax publicado",
+  detalhe: "O PR 2033 corrigiu a persistência do webhook Appmax e integrou no commit fee4d80691a916c77d04727fcf2acff821b5e182. Os nove checks passaram. O deploy desse commit terminou verde para pagamentos e admin. O aceite da auditoria permanece separado desta medição.",
+  autoridade: "github",
+  evidencia: "https://github.com/abundanciabr/sitesdoreino/pull/2033 https://github.com/abundanciabr/sitesdoreino/actions/runs/36002589535",
+  verificado_em: "2026-09-24",
+  precisa_do_dono: false,
+  responde_a: null,
+  gravidade: "info",
+  frente: "vender",
+  area: "pagamentos",
+  vence_em_dias: null
+});})();
