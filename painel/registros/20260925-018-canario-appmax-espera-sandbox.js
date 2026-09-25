7c0d8734b44c80c0cc0646be00019665ba6dcf2a
abundanciabr <abundanciabr@gmail.com>
2026-09-25 15:52:09 -0300

    painel: registrar que canario Appmax aguarda prova em sandbox

---
+++ painel/registros/20260925-018-canario-appmax-espera-sandbox.js
@@ -0,0 +1,17 @@
+(function(){ (window.REGISTROS = window.REGISTROS || []).push({
+  arquivo: "20260925-018-canario-appmax-espera-sandbox",
+  tipo: "pendencia",
+  quando: "2026-09-25",
+  titulo: "Canário Appmax espera prova do estorno em sandbox",
+  detalhe: "TAR-565 aguarda a conclusão do sandbox. TAR-644 ainda precisa comprovar, por resposta oficial autenticada, o valor efetivamente estornado e a identidade do pedido. A oferta real e o preço serão definidos após essa prova; cobrança e estorno reais exigem autorizações separadas.",
+  autoridade: "sessao",
+  evidencia: "https://github.com/abundanciabr/sitesdoreino/pull/2082",
+  verificado_em: "2026-09-25",
+  precisa_do_dono: false,
+  responde_a: null,
+  tarefa: "TAR-565",
+  gravidade: "ambar",
+  frente: "vender",
+  area: "pagamentos",
+  vence_em_dias: null
+}); })();
