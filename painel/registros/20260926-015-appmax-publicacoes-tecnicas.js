e5304d74db8d3cd1cc1bd797468b74623d2eedb9
abundanciabr <abundanciabr@gmail.com>
2026-09-26 00:19:29 -0300

    painel: registrar publicacoes tecnicas Appmax

---
+++ painel/registros/20260926-015-appmax-publicacoes-tecnicas.js
@@ -0,0 +1,16 @@
+(function(){ (window.REGISTROS = window.REGISTROS || []).push({
+  arquivo: "20260926-015-appmax-publicacoes-tecnicas",
+  tipo: "entrega",
+  quando: "2026-09-26",
+  titulo: "Compras: PRs 2097 e 2098 publicados",
+  detalhe: "O PR 2097 integrou Pix opaco; o deploy 36212482364 publicou. O JavaScript público respondeu HTTP 200 e passou em quatro casos sem cobrança. O PR 2098 integrou retomada segura; o deploy 36212981192 publicou. Provas técnicas sem compra ou estorno reais.",
+  autoridade: "github",
+  evidencia: "https://github.com/abundanciabr/sitesdoreino/pull/2097 https://github.com/abundanciabr/sitesdoreino/pull/2098 https://github.com/abundanciabr/sitesdoreino/actions/runs/36212482364 https://github.com/abundanciabr/sitesdoreino/actions/runs/36212981192 https://meshcraft.top/checkout/static/checkout/dados.js",
+  verificado_em: "2026-09-26",
+  precisa_do_dono: false,
+  responde_a: null,
+  gravidade: "verde",
+  frente: "vender",
+  area: "painel",
+  vence_em_dias: null
+});})();
