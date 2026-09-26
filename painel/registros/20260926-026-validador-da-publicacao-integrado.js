cf0ed7d12070e63eea0e8478a2c3c5a9116d61e9
abundanciabr <abundanciabr@gmail.com>
2026-09-26 10:32:03 -0300

    registro: escriturar integração do validador

---
+++ painel/registros/20260926-026-validador-da-publicacao-integrado.js
@@ -0,0 +1,8 @@
+(function(){ (window.REGISTROS = window.REGISTROS || []).push({
+ arquivo:"20260926-026-validador-da-publicacao-integrado",tipo:"entrega",quando:"2026-09-26",
+ titulo:"Validador da publicação integrado",
+ detalhe:"A correção da consulta à tentativa específica entrou na main pelo PR #2105 (head 78610e8a, merge adf3bf30). Os checks de integração passaram. A suíte do validador e do histórico GraphQL passou: 67 testes; a análise AST emitiu AST PASS.\n\nEste registro comprova a integração do código. Não afirma deploy nem publicação do site.",
+ autoridade:"github",evidencia:"https://github.com/abundanciabr/sitesdoreino/pull/2105",verificado_em:"2026-09-26",
+ precisa_do_dono:false,responde_a:null,relacao:"comentario",tarefa:"TAR-737",gravidade:"verde",frente:null,area:"ci",vence_em_dias:null,
+ porque_so_voce:null,proximo_passo:null,se_eu_nao_decidir:null,recomendacao:null,reversivel:null,impacto:null,portao:null
+});})();
