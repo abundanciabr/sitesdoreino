c5dab5e54158fa2beca69da9922481892d86195b
abundanciabr <abundanciabr@gmail.com>
2026-09-26 00:19:29 -0300

    painel: registrar publicacoes tecnicas Appmax

---
+++ painel/registros/20260926-030-pr2097-quatro-casos-publicos-comprovados.js
@@ -0,0 +1,14 @@
+(function(){ (window.REGISTROS = window.REGISTROS || []).push({
+  arquivo: "20260926-030-pr2097-quatro-casos-publicos-comprovados",
+  tipo: "resposta",
+  quando: "2026-09-26",
+  titulo: "PR 2097: quatro casos públicos comprovados",
+  detalhe: "Complemento ao registro 20260926-015. O PR 2097 documenta GET público HTTP 200, bytes idênticos ao commit integrado e SHA 1c022f862f978e9f1cef4e5753e7971d824ac645190404c09a85ed08df034ffe. O JavaScript servido passou nos quatro cenários de API fictícia: Pix Appmax, crypto indisponível, cartão e Pix não Appmax. Sem compra real.",
+  autoridade: "github",
+  evidencia: "https://github.com/abundanciabr/sitesdoreino/pull/2097 https://meshcraft.top/checkout/static/checkout/dados.js",
+  verificado_em: "2026-09-26",
+  precisa_do_dono: false,
+  responde_a: "20260926-015-appmax-publicacoes-tecnicas",
+  gravidade: "verde",
+  area: "painel"
+});})();
